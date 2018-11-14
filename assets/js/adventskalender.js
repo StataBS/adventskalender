30e37ac36640a75bb3c857ab478cb4ad6b4e907c
Jonas Bieri <jonas.bieri@gmail.com>
2018-11-14 15:31:16 +0000

    Initialize filter.js, show thumbnails, start with modal carousel

--- assets/js/adventskalender.js
+++ assets/js/adventskalender.js
@@ -2,29 +2,34 @@
 
 /*
 global $
-global d3
+global FilterJS
 */
 
 $(document).ready(function(){
+    
     $.get("data/adventskalender.tsv", function(data) {
-        
-        //see https://gist.github.com/iwek/7154706
-        function tsvToObject(tsv){
-          var lines = tsv.split("\n");
-          var result = [];
-          var headers = lines[0].split("\t");
-          for(var i = 1;i < lines.length; i++){
-        	  var obj = {};
-        	  var currentline=lines[i].split("\t");
-        	  for(var j = 0; j < headers.length; j++){
-        		  obj[headers[j]] = currentline[j];
-        	  }
-        	  result.push(obj);
-          }
-          return result;
-        }
-        
         var dataArray = tsvToObject(data);
         console.log(dataArray);
+        
+        var FJS = FilterJS(dataArray, '#days', {
+          template: '#day-template',
+        });
     });
 });
+
+
+//see https://gist.github.com/iwek/7154706
+function tsvToObject(tsv){
+  var lines = tsv.split("\n");
+  var result = [];
+  var headers = lines[0].split("\t");
+  for(var i = 1;i < lines.length; i++){
+	  var obj = {};
+	  var currentline=lines[i].split("\t");
+	  for(var j = 0; j < headers.length; j++){
+		  obj[headers[j]] = currentline[j];
+	  }
+	  result.push(obj);
+  }
+  return result;
+}
